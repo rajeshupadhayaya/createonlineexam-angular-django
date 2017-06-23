6e6cb2be87eda4340c96055ef0b39e90ddcc024e
Rajesh Upadhayaya <rajeshupadhayaya@gmail.com>
2017-06-24 00:51:39 +0530

    fix setting

--- exams/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url
-from exams.views import ObjectiveView
-# from . import views
-app_name = 'exams'
-
-urlpatterns = [
-    url(r'^objective/$', ObjectiveView.as_view(), name='objective'),
-]
